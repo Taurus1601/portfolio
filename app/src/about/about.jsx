13db86464c8babc198774e6af6418c4f9d0127ff
Sanjan Athyady <sanjanathyady8@gmail.com>
2024-05-08 02:45:40 +0530

    about and photos

--- app/src/about/about.jsx
+++ app/src/about/about.jsx
@@ -1,69 +1,88 @@
 import React from 'react'
-
+import "./about.css";
 function About() {
   return (
    
    <>
-<h1 className='font-semibold font-mono text-3xl m-10'>
+<h1 className='font-semibold font-mono text-6xl m-10'>
   About
 </h1>
-   <main className='flex flex-row w-[100vw] h-[80vh] bg-slate-700'>
-    <div id='details' className='bg-sky-500 w-[70vw] h-[80vh]'>
-      <main className='grid grid-cols-2 gap-5 m-5 mt-60'>
-            <div className="container bg-green-400 h-24 flex flex-row gap-2" id='language'>
-            <div id='image' className='bg-zinc-700 basis-1/3'><img src="#" alt="" /> </div>
-
-              <div id='details' className='bg-zinc-200 basis-80'>
-                <h1 className='font-semibold font-mono text-3xl m-10'></h1>
-                <p className='font-sans'>
-                </p>
-              </div>
-
-            </div>
-            <div className="container bg-green-400 h-24 flex flex-row gap-2" id='framework'>
-            <div id='image 'className='bg-zinc-700 basis-1/3'><img src="#" alt="" /> </div>
-
-              <div id='details' className='bg-zinc-200 basis-80'>
-                <h1 className='font-semibold font-mono text-3xl m-10'></h1>
-                <p className='font-sans'>
-                </p>
-              </div>
-            </div>
-            <div className="container bg-green-400 h-24 flex flex-row gap-2" id='Tools'>
-            <div id='image' className='bg-zinc-700 basis-1/3'><img src="#" alt="" /> </div>
-
-              <div id='details' className='bg-zinc-200 basis-80'>
-                <h1 className='font-semibold font-mono text-3xl m-10'></h1>
-                <p className='font-sans'>
-                </p>
-              </div>
-            </div>
-            <div className="container bg-green-400 h-24 flex flex-row gap-2" id='Platform'>
-            <div id='image' className='bg-zinc-700 basis-1/3'><img src="#" alt="" /> </div>
-
-              <div id='details' className='bg-zinc-200 basis-80'>
-                <h1 className='font-semibold font-mono text-3xl m-10'></h1>
-                <p className='font-sans'>
-                </p>
-              </div>
-
-            </div>
-            <div className="container bg-green-400 h-24 flex flex-row gap-2" id='soft skill'>
-              <div id='image' className='bg-zinc-700 basis-1/3'><img src="#" alt="" /> </div>
-
-              <div id='details' className='bg-zinc-200 basis-80'>
-                <h1 className='font-semibold font-mono text-3xl m-10'></h1>
-                <p className='font-sans'>
-                </p>
-              </div>
-
-            </div>
-            <div className="container bg-green-400 h-24 flex flex-row gap-2" id='experience'>
-            <div id='image' className='bg-zinc-700 basis-1/3'><img src="#" alt="" /> </div>
+   <main className='flex flex-row w-[100vw] h-[80vh] '>
+    <div id='details' className=' w-[70vw] h-[80vh]'>
+      <main className='grid grid-cols-2 gap-10 m-8 mt-28'>
+            <div className="container border rounded  h-32 flex flex-row gap-2" id='language'>
             
-              <div id='details' className='bg-zinc-200 basis-80' >
-                <h1 className='font-semibold font-mono text-3xl m-10'></h1>
-                <p className='font-sans'>
+            <div id='image' className=' basis-1/3 ml-5 -mr-10'>
+            <img className='w-16 mt-5' src="https://img.icons8.com/ios-filled/50/source-code.png" alt="source-code"/>
+               </div>
+
+              <div id='details' className=' basis-80'>
+                <h1 className='font-semibold font-mono text-3xl m-3 '> Languages</h1>
+                <p className='font-sans px-7 
+                '>
+                Python,C,JavaScript,
+                SQL,HTML/CSS,NoSQL.
+                </p>
+              </div>
+
+            </div>
+            <div className="container  border rounded  h-32 pl-5 flex flex-row gap-2" id='framework'>
+            
+            <div id='image 'className=' basis-1/3 '>
+            <img className='w-20 -ml-2 mt-2'
+             src="https://img.icons8.com/external-line-lima-studio/64/external-build-coding-line-lima-studio.png" alt="external-build-coding-line-lima-studio"/> </div>
+
+              <div id='details' className=' basis-80 -ml-10'>
+                <h1 className='font-semibold font-mono text-3xl m-3 ml'>Frameworks</h1>
+                <p className='font-sans px-7'>
+                React,NextJs,Tailwind,
+                FramerMotion,react-three-fiber.
+                </p>
+              </div>
+            </div>
+            <div className="container   border rounded  h-32 pt-5 flex flex-row gap-1" id='Tools'>
+            
+            <div id='image' className=' basis-1/3 -mr-10 ml-5'><img className='w-14 '
+            src="https://img.icons8.com/ios-filled/50/maintenance.png" alt="maintenance"/> </div>
+
+              <div id='details' className=' basis-80 ml-2'>
+                <h1 className='font-semibold font-mono text-3xl m-3 -mt-1'>Tools</h1>
+                <p className='font-sans px-7'>         Git,MySQL,Figma,Postman
+                </p>
+              </div>
+            </div>
+            <div className="container  border rounded  h-32 pt-5 pl-5 flex flex-row gap-2" id='Platform'>
+            
+            <div id='image' className=' basis-1/3 -mr-4'><img className='w-16' src="https://img.icons8.com/ios-filled/50/virtual-machine.png" alt="virtual-machine"/> </div>
+
+              <div id='details' className=' basis-80 -ml-5'>
+                <h1 className='font-semibold font-mono text-3xl m-3 '>Platforms</h1>
+                <p className='font-sans px-7'>
+                Linux,Windows,Firebase
+                </p>
+              </div>
+
+            </div>
+            <div className="container  border rounded  h-32 pt-5 flex flex-row gap-2" id='soft skill'>
+              <div id='image' className=' basis-1/3 ml-5 -mr-10'>
+              <img className='w-16' src="https://img.icons8.com/ios-filled/50/communication-skill.png" alt="communication-skill"/> </div>
+
+              <div id='details' className='basis-80 ml-4'>
+                <h1 className='font-semibold font-mono text-3xl '>soft skill</h1>
+                <p className='font-sans px-6'>
+                Leadership,Event Management,Teaching.
+                </p>
+              </div>
+
+            </div>
+            <div className="container  border rounded  h-32 pt-5 pl-5 flex flex-row gap-2" id='experience'>
+            <div id='image' className=' basis-1/3 -mr-4'>
+              <img className='w-16' src="https://img.icons8.com/ios/50/briefcase-settings.png" alt="briefcase-settings"/> </div>
+            
+              <div id='details' className=' basis-80 -ml-5' >
+                <h1 className='font-semibold font-mono text-3xl m-3'>Experience</h1>
+                <p className='font-sans px-7'>
+                  Web Developer Intern 
                 </p>
               </div>
 
@@ -71,19 +90,19 @@ function About() {
             
       </main>
     </div>
-    <div id="image">
+    <div id="image" className='bg-slate-500 basis-1/3 '>
 
     </div>
    </main>
-    <span className='bg-red-400 w-[100vw] h-[24vh] flex flex-col'>
-      <div id="resume" className='w-[100vw] h-[12vh] bg-amber-500'>
-         <p></p>
-         <button className='bg-white p-3  relative left-[80vw] rounded top-6'>Resume →</button>
+    <span className=' w-[100vw] h-[24vh] flex flex-col'>
+      <div id="resume" className='w-[100vw] h-[12vh] '>
+         <p className='top-10 relative text-xl text-center'> Check out my Resume here </p>
+         <button className='bg-white p-3  relative left-[80vw] rounded top-1'>Resume →</button>
 
       </div>
-      <div id="projects" className='w-[100vw] h-[12vh] bg-violet-700'>
-        <p></p>
-        <button className='bg-white p-3 relative left-[80vw] rounded top-6'>Projects →</button>
+      <div id="projects" className='w-[100vw] h-[12vh] '>
+      <p className='top-10 relative text-xl text-center'> Check out my Projects Here</p>
+        <button className='bg-zinc-300 p-3 relative left-[80vw] rounded top-1'>Projects →</button>
       </div>
     </span>
         
--- app/src/photos/photo.jsx
+++ app/src/photos/photo.jsx
@@ -3,54 +3,64 @@ import React, { useState } from "react";
 import "./photos.css";
 
 function Photos() {
- const handlechanges=()=>{}
+  const statc ='rotate-0 scale-[0.65]' ;
+  const statc2 = 'rotate-12 scale-[0.45]';
+  const [effect , setEffect ]= useState('rotate-0 scale-[0.60]');
+  const [effect1 , setEffect1 ]= useState('rotate-12 scale-[0.3]');
+  const [appear , setAppear] = useState("hidden");
+
+ const handlechanges=()=>{
+  {
+      setEffect('rotate-6 scale-[0.65]');
+      setEffect1('rotate-12 scale-[0.40]');
+      console.log("hello");
+      setAppear("visbile animate-pulse");
+  }
+ }
+ function handlechangesleave(){
+  setEffect('rotate-0 scale-[0.60]');
+  setEffect1('rotate-12 scale-[0.3]');
+  setAppear("");
+ }
   
   return (
     <>
-      <h1 className="mt-5 font-mono font-semibold text-4xl ml-3">Photos</h1>
+   
       
-      <main className="grid grid-cols-2 gap-0 mt-5  ml-2">
-        <div className="grid-div1 ">
-          <img src={"/public/IMG_0371.jpeg"} alt="" className="" />
-        </div>
 
-        <div className="grid-div2 "></div>
-        <div className="grid-div2 "></div>
+   <h1 className=" font-mono font-semibold text-4xl fixed top-0 bg-white h-[15vh] pt-10 px-10 pb-10 w-[100vw]">Photos</h1>
 
-        <div className="grid-div1">
-          <img src="/public/IMG_0918.jpeg" className="" alt="" />
-        </div>
-
-        <div className="grid-div1">
-          <img src="/public/IMG_0922.jpeg"  className="" alt="" />
-        </div>
-
-        <div className="grid-div2"></div>
-        <div className="grid-div2 "></div>
-
-        <div className="grid-div1 select-grid">
-          <span className='span1'>
-            <img src="/small/1.jpeg" />
-            <img src="/small/2.jpeg" />
-            <img src="/small/6.jpeg" />
-            <img src="/small/7.jpeg" />
-            <img src="/small/5.jpeg" />
-            <img src="/small/3.jpeg" />
-            <img src="/small/4.jpeg" />
-          </span>
-          <div
-            className="absolute z-[100] rounded-lg  text-center font-sans font-medium text-2xl text-white top-0
-           hover:bg-black/50 w-[100%] h-[100%] duration-500" onFocus={handlechanges}
-          >
-            <p
-              className=" m-14 w-[40vw] pl-[56px] pt-[130px]
-             h-[40vh] text-transparent hover:text-white
-              hover:scale-125 duration-500" onMouseEnter={handlechanges} onMouseLeave={handlechanges}
-            >
-              click here to see full gallery - &gt;
-            </p>
+      <main className="flex flex-rows w-[100vw] h-[100vh] translate-y-20 mt-10 overflow-x-hidden">
+        <div className="basis-1/2">
+          <div > <img src={"/public/IMG_0371.jpeg"} alt="" className="w-[100%]" /></div>
+          <div className=" h-[66vh]"></div>
+          <div >  <img src="/public/IMG_0922.jpeg"  className="w-[100%]" alt="" /></div>
+          <div className={`  text-center ${appear} translate-y-80`}>
+            Check My Collection 👉🏻<button className="p-3 mx-5  rounded bg-zinc-200">
+Click Here
+            </button>
           </div>
+
+
+        </div>
+        <div className="basis-1/2">
+          <div className=" h-[53vh]"></div>
+        <div > <img src="/public/IMG_0918.jpeg" className="w-[100%]" alt="" /></div>
+        <div className=" h-[49.5vh] "></div>
+        <div className="-m-3  h-[53vh] relative " onMouseEnter={handlechanges} onMouseLeave={handlechangesleave} >
+        <span className='flex relative  '>
+            <img src="/small/1.jpeg" className={`absolute  rounded-md -translate-x-[100px] -translate-y-12 rotate-0 z-10 ${effect} duration-500`}/>
+            <img src="/small/2.jpeg" className={`absolute  rounded-md translate-x-[248px] -translate-y-12 rotate-0 z-10 ${effect} duration-500`}/>
+            <img src="/small/6.jpeg" className={`absolute  rounded-md -translate-x-[100px] translate-y-40 rotate-0 z-10 ${effect} duration-500`}/>
+            <img src="/small/7.jpeg" className={`absolute  rounded-md translate-x-[250px] translate-y-40 rotate-0 z-10 ${effect} duration-500`}/>
+            <img src="/small/5.jpeg" className={`absolute  rounded-md translate-x-16 translate-y-0 rotate-0 z-50 scale-[.5] hover:rotate-3 duration-500`}/>
+            <img src="/small/3.jpeg" className={`absolute rounded-md -translate-x-24 -translate-y-48 -rotate-12 z-10 ${effect1} duration-500`}/>
+            <img src="/small/4.jpeg" className={`absolute rounded-md translate-x-56 -translate-y-44 rotate-12 z-10 ${effect1} duration-500`}/>
+          </span>
+
+
          
+        </div>
         </div>
       </main>
     </>
